85354cb8e49a0c95266d0092eea1a599a8f5964e
AugF <wangyp@smail.nju.edu.cn>
2021-01-12 16:48:52 +0800

    update

--- test/openmp_test/openmp_lock.cc
+++ test/openmp_test/openmp_lock.cc
@@ -1,30 +1,6 @@
 #include <time.h>
-#include <process.h>
 #include <omp.h>
 #include <stdio.h>
-
-void TestAtomic()
-{
-    clock_t t1,t2;
-    int i = 0;
-    volatile LONG a = 0;
-    
-    t1 = clock();
-    for( i = 0; i < 2000000; i++ )
-    {
-        InterlockedIncrement( &a);
-    }
-    t2 = clock();
-    printf("SingleThread, InterlockedIncrement 2,000,000: a = %ld, time = %ld\n", a, t2-t1);
-    t1 = clock();
-    #pragma omp parallel for
-    for( i = 0; i < 2000000; i++ )
-    {
-        InterlockedIncrement(&a);
-    }
-    t2 = clock();
-    printf("MultiThread, InterlockedIncrement 2,000,000: a = %ld, time = %ld\n", a, t2-t1);
-}
 
 void TestOmpLock()
 {
@@ -61,7 +37,6 @@ void TestOmpLock()
 
 int main(int argc, char* argv[])
 {
-    TestAtomic();
     TestOmpLock();
     return 0;
 }
