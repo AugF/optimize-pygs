49b598d681e2fc61bfb496cc9e788219667e12db
AugF <wangyunpan@outlook.com>
2021-01-12 15:09:38 +0800

    update

--- test/openmp_test/opm_time.cc
+++ test/openmp_test/opm_time.cc
@@ -1,25 +1,31 @@
+#include <iostream>
+#include <cstdio>
 #include <time.h>
-#include <stdio.h>
- 
-int main()
-{
-   clock_t start_t, end_t;
-   double total_t;
-   int i;
- 
-   start_t = clock();
-   printf("程序启动，start_t = %ld\n", start_t);
-    
-   printf("开始一个大循环，start_t = %ld\n", start_t);
-   for(i=0; i< 10000000; i++)
-   {
+#include <omp.h>
+using namespace std;
+
+void test() {
+   int a = 0;
+   clock_t t1 = clock();
+   for (int i = 0; i < 100000000; i ++) {
+      a = i + 1;
    }
-   end_t = clock();
-   printf("大循环结束，end_t = %ld\n", end_t);
-   
-   total_t = (double)(end_t - start_t) / CLOCKS_PER_SEC;
-   printf("CPU 占用的总时间：%f\n", total_t  );
-   printf("程序退出...\n");
- 
-   return(0);
+   clock_t t2 = clock();
+   printf("Time = %d\n", t2 - t1);
+}
+
+int main() {
+
+   printf("%d", CLOCKS_PER_SEC);
+
+   clock_t t1 = clock();
+#pragma omp parallel for
+   for (int j = 0; j < 2; j ++) {
+      test();
+   }
+   clock_t t2 = clock();
+   printf("Total time = %d\n", t2 - t1);
+
+   test();
+   return 0;
 }
