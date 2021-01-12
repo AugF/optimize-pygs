e20ace409ede10d2ed87f99823302f6269bd7f5b
AugF <wangyp@smail.nju.edu.cn>
2021-01-12 21:14:11 +0800

    update

---
+++ test/cpython_test/hello.pyx
@@ -0,0 +1,4 @@
+# https://cython.readthedocs.io/en/latest/src/quickstart/build.html
+def say_hello_to(name):
+    print("Hello %s!" % name)
+
---
+++ test/test_mpipe_2.py
@@ -0,0 +1,46 @@
+import time
+import sys
+from mpipe import OrderedStage, Pipeline
+
+if len(sys.argv) >= 5:
+    print("python file.py st1 st2 st3 total_num")
+    st1, st2, st3, total_num = float(sys.argv[1]), float(sys.argv[2]), float(sys.argv[3]), int(sys.argv[4])
+else:
+    st1, st2, st3 = 0.02, 0.01, 0.04
+    total_num = 10
+
+print(f"st1={st1}, st2={st2}, st3={st3}, total_num={total_num}")
+def increment(value):
+    time.sleep(st1)
+    return value + 1
+
+def double(value):
+    time.sleep(st2)
+    return value * 2
+
+def echo(value):
+    time.sleep(st3)
+    return value + 3
+
+t1 = time.time()
+stage1 = OrderedStage(increment)
+stage2 = OrderedStage(double)
+stage3 = OrderedStage(echo)
+stage1.link(stage2)
+stage2.link(stage3)
+pipe = Pipeline(stage1)
+
+for number in range(total_num):
+    pipe.put(number)
+
+pipe.put(None)
+
+for res in pipe.results():
+    print(res)
+print("use time: ", time.time() - t1, "except time: ", sum([st1, st2, st3]) + max([st1, st2, st3]) * (total_num - 1), "original time:", total_num * (st1 + st2 + st3))
+
+t1 = time.time()
+for i in range(total_num):
+    print(echo(double(increment(total_num))))
+print("origin time", time.time() - t1)
+    
